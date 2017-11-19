93c6d19d28d7640c63cd0232a430896c3c95cfe8
bmayle <bmayle17@gmail.com>
2017-11-19 09:42:17 -0500

    integer

---
+++ evenodd_test.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require_relative "evenodd.rb"
+class TestEvenOdd < Minitest::Test
+
+	def test_assert_that_1_equals_1
+        assert_equal(1, 1)
+    end
+
+    def test_assert_that_number_is_integer
+    	assert_equal(Integer, getnumber())
+    end
+end
